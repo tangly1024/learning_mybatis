9ff66f6982b47106952321edc4e299f0726085db
tlyong1992 <tlyong1992@hotmail.com>
2018-04-14 19:51:56 +0800

    用thymeleaf显示

--- src/main/java/com/troy1024/controller/UserController.java
+++ src/main/java/com/troy1024/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,8 +24,10 @@ public class UserController {
 
     @RequestMapping("/list")
     @ResponseBody
-    public Object listall(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView listall(HttpServletRequest request, HttpServletResponse response) {
         List<Article> articles = userMapper.getUserArticlesByUserId(1);
-        return articles;
+        ModelAndView mav = new ModelAndView("list");
+        mav.addObject("articles", articles);
+        return mav;
     }
 }
